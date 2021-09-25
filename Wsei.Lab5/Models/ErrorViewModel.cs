b37c726a0e27358e6255e180773554175b806986
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2021-09-25 14:14:22 +0200

    Add project files.

---
+++ Wsei.Lab5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Wsei.Lab5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
