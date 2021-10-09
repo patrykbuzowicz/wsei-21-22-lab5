6333873294e07cc64b973626681be52ed70e76bc
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2021-10-09 14:36:23 +0200

    new Add Product form

---
+++ Wsei.Lab5/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wsei.Lab5.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wsei.Lab5/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Wsei.Lab5.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
