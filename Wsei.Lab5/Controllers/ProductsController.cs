0482c66a905fefa441f534442ff6a0e2257b3cca
Patryk Buzowicz <patryk_buzowicz@hotmail.com>
2021-11-06 14:34:03 +0100

    add saving to database

--- Wsei.Lab5/Controllers/ProductsController.cs
+++ Wsei.Lab5/Controllers/ProductsController.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Wsei.Lab5.Database;
+using Wsei.Lab5.Entities;
+using Wsei.Lab5.Models;
 
 namespace Wsei.Lab5.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly AppDbContext _dbContext;
+
+        public ProductsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Add()
+        public async Task<IActionResult> Add(ProductModel product)
         {
+            var entity = new ProductEntity
+            {
+                Name = product.Name,
+                Description = product.Description,
+                IsVisible = product.IsVisible,
+            };
+
+            await _dbContext.Products.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
             return View();
         }
     }
